36d30f45f1788162ec665196a3f6ae4baefc8719
Ahira Adefokun <justiceahira@gmail.com>
2023-02-12 21:29:55 +0100

    Add openapi docs generation

--- src/main/java/com/ahirajustice/mockserver/modules/mock/controllers/MockController.java
+++ src/main/java/com/ahirajustice/mockserver/modules/mock/controllers/MockController.java
@@ -1,7 +1,13 @@
 package com.ahirajustice.mockserver.modules.mock.controllers;
 
+import com.ahirajustice.mockserver.common.error.ErrorResponse;
 import com.ahirajustice.mockserver.modules.mock.models.SaveEndpointRequest;
 import com.ahirajustice.mockserver.modules.mock.services.MockService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,14 +24,28 @@ public class MockController {
 
     private final MockService mockService;
 
+    @Operation(summary = "Save Endpoint")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", content = { @Content(mediaType = "application/json") }),
+                    @ApiResponse(responseCode = "403", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)) })
+            }
+    )
     @RequestMapping(path = "/endpoint", method = RequestMethod.POST)
     public void saveEndpoint(@RequestBody @Valid SaveEndpointRequest request) {
         mockService.saveEndpoint(request);
     }
 
+    @Operation(summary = "Handle Request")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", content = { @Content(mediaType = "application/json") }),
+                    @ApiResponse(responseCode = "404", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)) })
+            }
+    )
     @RequestMapping(
             path = "/**",
-            method = {RequestMethod.OPTIONS, RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.PATCH}
+            method = { RequestMethod.OPTIONS, RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.PATCH }
     )
     public ResponseEntity<Object> handleRequest(HttpServletRequest request) {
         return mockService.handleRequest(request);
